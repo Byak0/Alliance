e840fda24d8228134c30032c546bc512984ec477
Byako <lkoinw@hotmail.fr>
2023-11-27 20:02:32 +0100

    Use Reflection to increase native limits since fields are now static readonly.

--- Alliance.Common/Patch/DirtyCommonPatcher.cs
+++ Alliance.Common/Patch/DirtyCommonPatcher.cs
@@ -1,4 +1,6 @@
 ﻿using Alliance.Common.Patch.HarmonyPatch;
+using System.Reflection;
+using TaleWorlds.MountAndBlade;
 using static Alliance.Common.Utilities.Logger;
 
 namespace Alliance.Common.Patch
@@ -11,21 +13,34 @@ namespace Alliance.Common.Patch
     {
         public static void IncreaseNativeLimits()
         {
-            //// Increase the limit for number of bots in captain (255 to 16384)
-            //CompressionMission.AgentOffsetCompressionInfo = new CompressionInfo.Integer(0, 16384, true);
-            //// Increase map time limit (in minutes)
-            //CompressionBasic.MapTimeLimitCompressionInfo = new CompressionInfo.Integer(0, 360, true);
-            //// Increase network message size for round time (in seconds)
-            //CompressionMission.RoundTimeCompressionInfo = new CompressionInfo.Integer(0, 3600, true);
-            //// Increase formation max
-            //CompressionBasic.NumberOfBotsPerFormationCompressionInfo = new CompressionInfo.Integer(0, 4096, true);
-            //// Increase gold max
-            //CompressionBasic.RoundGoldAmountCompressionInfo = new CompressionInfo.Integer(-1, 50000, true);
-            //// Fix native lag when lot of arrows
-            //CompressionBasic.BigRangeLowResLocalPositionCompressionInfo = new CompressionInfo.Float(-2000f, 2000f, 16);
+            // Increase the limit for number of bots in captain (255 to 16384)
+            typeof(CompressionMission).GetField(nameof(CompressionMission.AgentOffsetCompressionInfo), BindingFlags.Static).
+                SetValue(null, new CompressionInfo.Integer(0, 16384, true));
 
-            //// Test :)))
-            //CompressionBasic.PlayerCompressionInfo = new CompressionInfo.Integer(-1, 20000, true);
+            // Increase map time limit (in minutes)
+            typeof(CompressionBasic).GetField(nameof(CompressionBasic.MapTimeLimitCompressionInfo), BindingFlags.Static).
+                SetValue(null, new CompressionInfo.Integer(0, 360, true));
+
+            // Increase network message size for round time (in seconds)
+            typeof(CompressionMission).GetField(nameof(CompressionMission.RoundTimeCompressionInfo), BindingFlags.Static).
+                SetValue(null, new CompressionInfo.Integer(0, 3600, true));
+
+            // Increase formation max
+            typeof(CompressionBasic).GetField(nameof(CompressionBasic.NumberOfBotsPerFormationCompressionInfo), BindingFlags.Static).
+                SetValue(null, new CompressionInfo.Integer(0, 4096, true));
+
+            // Increase gold max
+            typeof(CompressionBasic).GetField(nameof(CompressionBasic.RoundGoldAmountCompressionInfo), BindingFlags.Static).
+                SetValue(null, new CompressionInfo.Integer(-1, 50000, true));
+
+            // TODO : Check if still necessary with 1.2 optimisation
+            // Fix native lag when lot of arrows
+            //typeof(CompressionBasic).GetField(nameof(CompressionBasic.BigRangeLowResLocalPositionCompressionInfo), BindingFlags.Static).
+            //    SetValue(null, new CompressionInfo.Float(-2000f, 2000f, 16));
+
+            // Test :))) => Cool but what's the purpose of this
+            //typeof(CompressionBasic).GetField(nameof(CompressionBasic.PlayerCompressionInfo), BindingFlags.Static).
+            //    SetValue(null, new CompressionInfo.Integer(-1, 20000, true));
         }
 
         public static bool Patch()
