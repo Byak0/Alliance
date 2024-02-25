48a4d887da3282d69e7dcaa1d8605b6a26afb67c
Byako <byako.dev@outlook.fr>
2024-02-26 00:38:43 +0100

    Make IA more aggressive.

--- Alliance.Server/Extensions/AIBehavior/ALTeamAIGeneral.cs
+++ Alliance.Server/Extensions/AIBehavior/ALTeamAIGeneral.cs
@@ -1,4 +1,7 @@
-﻿using TaleWorlds.Core;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using TaleWorlds.Core;
 using TaleWorlds.MountAndBlade;
 
 namespace Alliance.Server.Extensions.AIBehavior
@@ -9,9 +12,90 @@ namespace Alliance.Server.Extensions.AIBehavior
     /// </summary>
     public class ALTeamAIGeneral : TeamAIGeneral
     {
+        private readonly float _occasionalTickTime;
+        private MissionTime _nextTacticChooseTime;
+        private MissionTime _nextOccasionalTickTime;
+        private FieldInfo _availableTacticsFI;
+        private PropertyInfo _getIsFirstTacticChosenPI;
+        private PropertyInfo _currentTacticPI;
+        private PropertyInfo _isDefenseApplicablePI;
+        private MethodInfo _getTacticWeightMI;
+        private MethodInfo _resetTacticalPositionsMI;
+
+        public FieldInfo AvailableTacticsFI
+        {
+            get
+            {
+                if (_availableTacticsFI == null)
+                {
+                    _availableTacticsFI = typeof(TeamAIComponent).GetField("_availableTactics", BindingFlags.Instance | BindingFlags.NonPublic);
+                }
+                return _availableTacticsFI;
+            }
+        }
+
+        public PropertyInfo GetIsFirstTacticChosenPI
+        {
+            get
+            {
+                if (_getIsFirstTacticChosenPI == null)
+                {
+                    _getIsFirstTacticChosenPI = typeof(TeamAIComponent).GetProperty("GetIsFirstTacticChosen", BindingFlags.Instance | BindingFlags.Public);
+                }
+                return _getIsFirstTacticChosenPI;
+            }
+        }
+
+        public PropertyInfo CurrentTacticPI
+        {
+            get
+            {
+                if (_currentTacticPI == null)
+                {
+                    _currentTacticPI = typeof(TeamAIComponent).GetProperty("CurrentTactic", BindingFlags.Instance | BindingFlags.NonPublic);
+                }
+                return _currentTacticPI;
+            }
+        }
+
+        public PropertyInfo IsDefenseApplicablePI
+        {
+            get
+            {
+                if (_isDefenseApplicablePI == null)
+                {
+                    _isDefenseApplicablePI = typeof(TeamAIComponent).GetProperty("IsDefenseApplicable", BindingFlags.Instance | BindingFlags.Public);
+                }
+                return _isDefenseApplicablePI;
+            }
+        }
+
+        public MethodInfo GetTacticWeightMI
+        {
+            get
+            {
+                if (_getTacticWeightMI == null)
+                {
+                    _getTacticWeightMI = typeof(TacticComponent).GetMethod("GetTacticWeight", BindingFlags.NonPublic | BindingFlags.Instance);
+                }
+                return _getTacticWeightMI;
+            }
+        }
+
+        public MethodInfo ResetTacticalPositionsMI
+        {
+            get
+            {
+                if (_resetTacticalPositionsMI == null)
+                {
+                    _resetTacticalPositionsMI = typeof(TacticComponent).GetMethod("ResetTacticalPositions", BindingFlags.NonPublic | BindingFlags.Instance);
+                }
+                return _resetTacticalPositionsMI;
+            }
+        }
+
         public ALTeamAIGeneral(Mission currentMission, Team currentTeam, float thinkTimerTime = 10f, float applyTimerTime = 1f) : base(currentMission, currentTeam, thinkTimerTime, applyTimerTime)
         {
-
         }
 
         public override void OnUnitAddedToFormationForTheFirstTime(Formation formation)
@@ -56,7 +140,7 @@ namespace Alliance.Server.Extensions.AIBehavior
                 switch (formation.PhysicalClass)
                 {
                     case FormationClass.Infantry:
-                        formation.AI.SetBehaviorWeight<BehaviorAdvance>(1f);
+                        formation.AI.SetBehaviorWeight<BehaviorCharge>(1f);
                         break;
                     case FormationClass.Ranged:
                         formation.AI.SetBehaviorWeight<BehaviorHoldHighGround>(1f);
@@ -74,6 +158,117 @@ namespace Alliance.Server.Extensions.AIBehavior
                     case FormationClass.HeavyCavalry: break;
                 }
             }
+        }
+
+        protected override void Tick(float dt)
+        {
+            if (Team.BodyGuardFormation != null && Team.BodyGuardFormation.CountOfUnits > 0 && (Team.GeneralsFormation == null || Team.GeneralsFormation.CountOfUnits == 0))
+            {
+                Team.BodyGuardFormation.AI.ResetBehaviorWeights();
+                Team.BodyGuardFormation.AI.SetBehaviorWeight<BehaviorCharge>(1f);
+            }
+            if (_nextTacticChooseTime.IsPast)
+            {
+                MakeDecision();
+                _nextTacticChooseTime = MissionTime.SecondsFromNow(5f);
+            }
+            if (_nextOccasionalTickTime.IsPast)
+            {
+                TickOccasionally();
+                _nextOccasionalTickTime = MissionTime.SecondsFromNow(_occasionalTickTime);
+            }
+        }
+
+        private void MakeDecision()
+        {
+            List<TacticComponent> availableTactics = (List<TacticComponent>)AvailableTacticsFI.GetValue(this);
+            if ((Mission.CurrentState != Mission.State.Continuing && availableTactics.Count == 0) || !Team.HasAnyFormationsIncludingSpecialThatIsNotEmpty())
+            {
+                return;
+            }
+            bool flag = true;
+            foreach (Team team in Mission.Teams)
+            {
+                if (team.IsEnemyOf(Team) && team.HasAnyFormationsIncludingSpecialThatIsNotEmpty())
+                {
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag)
+            {
+                if (Mission.MissionEnded)
+                {
+                    return;
+                }
+                if (!(CurrentTactic is TacticCharge))
+                {
+                    foreach (TacticComponent tacticComponent in availableTactics)
+                    {
+                        if (tacticComponent is TacticCharge)
+                        {
+                            if (CurrentTactic == null)
+                            {
+                                GetIsFirstTacticChosenPI.SetValue(this, true);
+                            }
+                            CurrentTacticPI.SetValue(this, tacticComponent);
+                            break;
+                        }
+                    }
+                    if (!(CurrentTactic is TacticCharge))
+                    {
+                        if (CurrentTactic == null)
+                        {
+                            GetIsFirstTacticChosenPI.SetValue(this, true);
+                        }
+                        CurrentTacticPI.SetValue(this, availableTactics.FirstOrDefault());
+                    }
+                }
+            }
+            CheckIsDefenseApplicable();
+            MethodInfo GetTacticWeightMI = typeof(TacticComponent).GetMethod("GetTacticWeight", BindingFlags.NonPublic | BindingFlags.Instance);
+            TacticComponent tacticComponent2 = TaleWorlds.Core.Extensions.MaxBy(availableTactics, (TacticComponent to) => (float)GetTacticWeightMI.Invoke(to, null) * ((to == CurrentTactic) ? 1.5f : 1f));
+            bool flag2 = false;
+            if (CurrentTactic == null)
+            {
+                flag2 = true;
+            }
+            else if (CurrentTactic != tacticComponent2)
+            {
+                if (!(bool)ResetTacticalPositionsMI.Invoke(CurrentTactic, null))
+                {
+                    flag2 = true;
+                }
+                else
+                {
+                    float tacticWeight = (float)GetTacticWeightMI.Invoke(tacticComponent2, null);
+                    float num = (float)GetTacticWeightMI.Invoke(CurrentTactic, null) * 1.5f;
+                    if (tacticWeight > num)
+                    {
+                        flag2 = true;
+                    }
+                }
+            }
+            if (flag2)
+            {
+                if (CurrentTactic == null)
+                {
+                    GetIsFirstTacticChosenPI.SetValue(this, true);
+                }
+                CurrentTacticPI.SetValue(this, tacticComponent2);
+                if (Mission.Current.MainAgent != null && Team.GeneralAgent != null && Team.IsPlayerTeam && Team.IsPlayerSergeant)
+                {
+                    string name = tacticComponent2.GetType().Name;
+                    MBInformationManager.AddQuickInformation(GameTexts.FindText("str_team_ai_tactic_text", name), 4000, Team.GeneralAgent.Character, "");
+                }
+            }
+        }
+
+        public new void CheckIsDefenseApplicable()
+        {
+            // Always set to false to force AI to be aggresive
+            IsDefenseApplicablePI.SetValue(this, false);
+            return;
         }
     }
 }
