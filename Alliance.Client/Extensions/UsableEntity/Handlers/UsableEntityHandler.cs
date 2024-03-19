89ba9728e31a3e3601450b8a31ebab169b9baad8
P-H Lemp <pierrehenrilempereur1997@gmail.com>
2024-03-19 19:54:59 +0100

    Add reset system

--- Alliance.Client/Extensions/UsableEntity/Handlers/UsableEntityHandler.cs
+++ Alliance.Client/Extensions/UsableEntity/Handlers/UsableEntityHandler.cs
@@ -2,6 +2,7 @@
 using Alliance.Common.Extensions.UsableEntity.Behaviors;
 using Alliance.Common.Extensions.UsableEntity.NetworkMessages.FromServer;
 using System;
+using TaleWorlds.Engine;
 using TaleWorlds.MountAndBlade;
 using static Alliance.Common.Utilities.Logger;
 using static TaleWorlds.MountAndBlade.GameNetwork;
@@ -23,17 +24,32 @@ namespace Alliance.Client.Extensions.UsableEntity.Handlers
         public void Register(NetworkMessageHandlerRegisterer reg)
         {
             reg.Register<RemoveEntity>(RemoveEntity);
+            reg.Register<Reset>(Reset);
         }
 
         public void RemoveEntity(RemoveEntity message)
         {
             try
             {
-                GameMode.RemoveEntity(GameMode.FindClosestUsableEntity(message.Position, 0.1f));
+                GameEntity closestEntityToRemove = GameMode.FindClosestUsableEntity(message.Position, 0.05f);
+                GameMode.HideEntity(closestEntityToRemove);
             }
             catch (Exception ex)
             {
                 Log($"Alliance - Failed to remove entity at {message.Position}", LogLevel.Error);
+                Log(ex.ToString(), LogLevel.Error);
+            }
+        }
+
+        public void Reset(Reset message)
+        {
+            try
+            {
+                GameMode.ResetItemsWithTagRespawnEachRound();
+            }
+            catch (Exception ex)
+            {
+                Log("Alliance - Failed to reset usable items ", LogLevel.Error);
                 Log(ex.ToString(), LogLevel.Error);
             }
         }
--- Alliance.Common/Extensions/UsableEntity/Behaviors/UsableEntityBehavior.cs
+++ Alliance.Common/Extensions/UsableEntity/Behaviors/UsableEntityBehavior.cs
@@ -18,10 +18,18 @@ namespace Alliance.Common.Extensions.UsableEntity.Behaviors
     {
         private List<GameEntity> _usableEntities = new List<GameEntity>();
 
+        readonly MultiplayerRoundController roundController;
+
         public override void OnBehaviorInitialize()
         {
             base.OnBehaviorInitialize();
             _usableEntities = Mission.Current.Scene.FindEntitiesWithTag(AllianceTags.InteractiveTag).ToList();
+            MultiplayerRoundController roundController = Mission.GetMissionBehavior<MultiplayerRoundController>();
+
+            if (roundController != null)
+            {
+                roundController.OnRoundStarted += ResetItemsWithTagRespawnEachRound;
+            }
         }
 
         public void UseEntity(GameEntity entity, Agent agent)
@@ -37,12 +45,21 @@ namespace Alliance.Common.Extensions.UsableEntity.Behaviors
             agent.EquipWeaponWithNewEntity(slot, ref missionWeapon);
             agent.TryToWieldWeaponInSlot(slot, Agent.WeaponWieldActionType.WithAnimation, true);
 
-            RemoveEntity(entity);
+            HideEntity(entity);
 
             Log($"Agent {agent.Name} ({agent.MissionPeer?.Name}) used entity {entity.Name} and equipped {itemName}", LogLevel.Debug);
         }
 
-        public void RemoveEntity(GameEntity entity)
+        public override void OnRemoveBehavior()
+        {
+            if (roundController != null)
+            {
+                roundController.OnRoundStarted -= ResetItemsWithTagRespawnEachRound;
+            }
+            base.OnRemoveBehavior();
+        }
+
+        public void HideEntity(GameEntity entity)
         {
             if (entity == null) return;
 
@@ -53,8 +70,28 @@ namespace Alliance.Common.Extensions.UsableEntity.Behaviors
                 GameNetwork.EndBroadcastModuleEvent(GameNetwork.EventBroadcastFlags.None);
             }
 
-            entity.Remove(0);
-            _usableEntities.Remove(entity);
+            entity.SetVisibilityExcludeParents(false);
+        }
+
+        public void ResetItemsWithTagRespawnEachRound()
+        {
+            List<GameEntity> itemsToRespawnList = Mission.Current.Scene.FindEntitiesWithTag(AllianceTags.ENTITY_TO_RESPAWN_ON_EACH_ROUND_TAG).ToList();
+
+            if (GameNetwork.IsServer)
+            {
+                // As client to make them visible again (Code above in the foreach)
+                GameNetwork.BeginBroadcastModuleEvent();
+                GameNetwork.WriteMessage(new Reset());
+                GameNetwork.EndBroadcastModuleEvent(GameNetwork.EventBroadcastFlags.None);
+            }
+
+            itemsToRespawnList.ForEach(gameEntity =>
+            {
+                if (!gameEntity.IsVisibleIncludeParents())
+                {
+                    gameEntity.SetVisibilityExcludeParents(true);
+                }
+            });
         }
 
         public GameEntity FindClosestUsableEntity(Vec3 position, float range)
@@ -80,7 +117,7 @@ namespace Alliance.Common.Extensions.UsableEntity.Behaviors
             foreach (GameEntity entity in _usableEntities)
             {
                 // Check if the entity is in the direction the agent is looking
-                if (IsEntityInLookDirection(entity, eyePosition, lookDirection, out float distanceSquared))
+                if (IsEntityInLookDirection(entity, eyePosition, lookDirection, out float distanceSquared) && entity.IsVisibleIncludeParents())
                 {
                     if (distanceSquared < closestDistanceSquared)
                     {
@@ -89,7 +126,6 @@ namespace Alliance.Common.Extensions.UsableEntity.Behaviors
                     }
                 }
             }
-
             return closestEntity;
         }
 
