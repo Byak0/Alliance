2f05d247bded0e7571813ccd27fac2a7d5b6f6bb
generalgui <cogneau.guillaume@aliceadsl.fr>
2024-05-08 20:00:54 +0200

    WargAttack First try
    Working version with a horse as PlaceHolder.
    Work done :
    - Identified an agent within X° in front of the Mount
    - Apply damage to the agent list in front of the mount. (Currently AOE, need changes if mono target needed)
    - Trigger the competence based on condition (User is on a mount + cooldown)
    
    To do :
    - Link competence with Warg mount type.
    - Link Warg's animation & sound with the competence (Placeholder set with "horse kick" only on client side, will need to improve for multiplayer sync)

---
+++ Alliance.Client/Extensions/WargAttack/View/WargAttackView.cs
@@ -0,0 +1,62 @@
+﻿using Alliance.Common.Extensions.WargAttack.NetworkMessages.FromClient;
+using System;
+using TaleWorlds.MountAndBlade;
+using TaleWorlds.MountAndBlade.View;
+using TaleWorlds.MountAndBlade.View.MissionViews;
+using static Alliance.Common.Utilities.Logger;
+
+namespace Alliance.Client.Extensions.WargAttack.View
+{
+    [DefaultView]
+    public class WargAttackView : MissionView
+    {
+        private float cooldownDuration = 1.0f; // Durée du cooldown en secondes
+        private DateTime lastAttackTime = DateTime.MinValue; // Initialise le dernier moment où l'attaque a été utilisée à une valeur minimale
+
+
+        public WargAttackView() { }
+        public override void OnMissionScreenTick(float dt)
+        {
+            base.OnMissionScreenTick(dt);
+            if (Agent.Main != null)
+            {
+                //Modification à faire : Verifier touche + si joueur sur un Warg. Pour l'instant vérifie uniquement si sur une monture
+                if (Agent.Main.HasMount && Input.IsKeyPressed(TaleWorlds.InputSystem.InputKey.Q))
+                {
+                    if (CanUseWargAttack())
+                    {
+                        WargAttack();
+                        lastAttackTime = DateTime.UtcNow; // Met à jour le dernier moment où l'attaque a été utilisée                            
+                    }
+                    else
+                    {
+                        Log($"Warg Attack on cooldown! {TimeSpan.FromSeconds(cooldownDuration) - (DateTime.UtcNow - lastAttackTime)} secondes remaining", LogLevel.Debug);
+                    }
+
+                }
+            }
+        }
+
+        private bool CanUseWargAttack()
+        {
+            // Vérifie si suffisamment de temps s'est écoulé depuis la dernière utilisation
+            return DateTime.UtcNow - lastAttackTime >= TimeSpan.FromSeconds(cooldownDuration);
+        }
+
+        public void WargAttack()
+        {
+
+
+            Log($"Using WargAttack !", LogLevel.Debug);            
+            
+            //TODO
+            //Test pour jouer une action avec la fonctionnalité attaque. Devra être refait pour partie multijoueur
+            ActionIndexCache myAction = ActionIndexCache.Create("act_horse_kick");
+            Agent.Main.MountAgent.SetActionChannel(0, myAction, true);
+
+            GameNetwork.BeginModuleEventAsClient();
+            GameNetwork.WriteMessage(new RequestWargAttack()); // Envoi message au Common
+            GameNetwork.EndModuleEventAsClient();
+        }
+    }
+}
---
+++ Alliance.Common/Extensions/WargAttack/NetworkMessages/FromClient/RequestWargAttack.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaleWorlds.Core;
+using TaleWorlds.MountAndBlade;
+using TaleWorlds.MountAndBlade.Network.Messages;
+
+namespace Alliance.Common.Extensions.WargAttack.NetworkMessages.FromClient
+{
+    [DefineGameNetworkMessageTypeForMod(GameNetworkMessageSendType.FromClient)]
+    public sealed class RequestWargAttack : GameNetworkMessage
+    {
+        static readonly CompressionInfo.Integer EquipmentCompressionInfo = new CompressionInfo.Integer((int)EquipmentIndex.WeaponItemBeginSlot, (int)EquipmentIndex.NumEquipmentSetSlots, true);
+
+        //public EquipmentIndex EquipmentIndex { get; private set; }
+
+        public RequestWargAttack() { }
+
+        //public RequestUseItem(EquipmentIndex equipmentIndex)
+        //{
+        //    EquipmentIndex = equipmentIndex;
+        //}
+
+        protected override void OnWrite()
+        {
+            //WriteIntToPacket((int)EquipmentIndex, EquipmentCompressionInfo);
+        }
+
+        protected override bool OnRead()
+        {
+            bool bufferReadValid = true;
+            //EquipmentIndex = (EquipmentIndex)ReadIntFromPacket(EquipmentCompressionInfo, ref bufferReadValid);
+            return bufferReadValid;
+        }
+
+        protected override MultiplayerMessageFilter OnGetLogFilter()
+        {
+            return MultiplayerMessageFilter.General;
+        }
+
+        protected override string OnGetLogFormat()
+        {
+            return "Request usage of WarkAttack.";
+        }
+    }
+}
