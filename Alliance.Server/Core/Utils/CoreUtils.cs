48e5d43941d6fdf2c00a31881507bfd4af97793a
generalgui <cogneau.guillaume@aliceadsl.fr>
2024-09-09 20:56:19 +0200

    POC pour détecter la collision entre patte du warg et un bone du target le plus proche. Collision avec tête Warg ne semble pas fonctionner => à vérifier Gestion du refresh collision pendant animation en delay => à modifier en tick
    A clean et remettre en forme pour une version "propre" + ajouter les bones du warg dans une liste

--- Alliance.Server/Core/Utils/CoreUtils.cs
+++ Alliance.Server/Core/Utils/CoreUtils.cs
@@ -100,7 +100,7 @@ namespace Alliance.Server.Core.Utils
         }
 
         /// <summary>
-        /// Return the list of all agents alives that are near the target.
+        /// Return the list of all agents alive that are near the target.
         /// IT WILL NOT INCLUDE THE MOUNT OF THE TARGET IF THE TARGET IS MOUNTED
         /// </summary>
         /// <param name="range"></param>
@@ -119,6 +119,7 @@ namespace Alliance.Server.Core.Utils
                 // Do not include mount of player and player.
                 if (agent == target.MountAgent || agent == target) continue;
 
+                //float distance = agent.Position.Distance(target.Position);
                 float distance = agent.Position.Distance(target.Position);
 
                 //Add offset in case of mount since mount are large
@@ -134,6 +135,43 @@ namespace Alliance.Server.Core.Utils
             }
 
             return agentsInRange;
+        }
+
+        /// <summary>
+        /// Determine nearest agent alive from the target
+        /// IT WILL NOT INCLUDE THE MOUNT OF THE TARGET IF THE TARGET IS MOUNTED
+        /// </summary>
+        /// <param name="range">Ranger of search</param>
+        /// <param name="target">eg : Current player</param>
+        /// <returns>Nearest agent but not current player's mount</returns>
+        public static Agent GetNearestAliveAgentInRange(float range, Agent target)
+        {
+            MBList<Agent> nearbyAgents = new MBList<Agent>();
+            Mission.Current.GetNearbyAgents(target.Position.AsVec2,range,nearbyAgents);
+            Agent closestAgentInRange = null;
+            
+
+            float closestDistanceSquared = float.MaxValue;//To be sure the first agent range is always at lower range value
+
+            foreach (Agent agent in nearbyAgents)
+            {
+                if (!agent.IsActive()) continue;
+
+                // Do not include mount of player and player.
+                if (agent == target.MountAgent || agent == target) continue;
+
+                //float distance = agent.Position.Distance(target.Position);
+                float distance = (agent.Position - target.Position).LengthSquared;// Work with square length. Better perf         
+
+                //Keep in memory the closest agent
+                if (distance < closestDistanceSquared)
+                {
+                    closestDistanceSquared = distance;
+                   closestAgentInRange = agent;
+                }
+            }
+
+            return closestAgentInRange;
         }
     }
 }
--- Alliance.Server/Extensions/WargAttack/Handlers/WargAttackHandler.cs
+++ Alliance.Server/Extensions/WargAttack/Handlers/WargAttackHandler.cs
@@ -5,7 +5,6 @@ using Alliance.Common.Extensions.AnimationPlayer.Models;
 using Alliance.Common.Extensions.WargAttack.NetworkMessages.FromClient;
 using Alliance.Server.Core.Utils;
 using System;
-using System.Collections.Generic;
 using System.Threading.Tasks;
 using TaleWorlds.Core;
 using TaleWorlds.Library;
@@ -30,7 +29,6 @@ namespace Alliance.Server.Extensions.WargAttack.Handlers
             }
 
             Agent mountAgent = peer.ControlledAgent.MountAgent;
-            int waitTime = 800;//1000 = 1s. Temps d'attente entre début animation en mouvement et le dégat
 
             //On détermine si on joue l'animation attaque en mouvement(avec un cooldown à l'exec du degat) ou attaque statique en se basant sur la velocité
             if (mountAgent.MovementVelocity.Y >= 4)
@@ -38,123 +36,198 @@ namespace Alliance.Server.Extensions.WargAttack.Handlers
                 Animation animation = AnimationSystem.Instance.DefaultAnimations.Find(anim => anim.Name == "act_warg_attack_running");
                 AnimationSystem.Instance.PlayAnimation(mountAgent, animation, true);
 
-                wargAttack(peer, waitTime);
+                WargAttack(peer, 22);
             }
             else
             {
                 Animation animation = AnimationSystem.Instance.DefaultAnimations.Find(anim => anim.Name == "act_warg_attack_stand");
                 AnimationSystem.Instance.PlayAnimation(mountAgent, animation, true);
 
-                wargAttack(peer, 0);
+                WargAttack(peer, 36);
             }
 
             return true;
         }
 
         // Détection agent autour du joueur et application dégats
-        public async void wargAttack(NetworkCommunicator peer, int waitTime)
+        public async void WargAttack(NetworkCommunicator peer, int boneType)
         {
-            await Task.Delay(waitTime);
-
             if (peer.ControlledAgent == null || peer.ControlledAgent.MountAgent == null) return;
-
-            Vec3 PlayerPosition = peer.ControlledAgent.Position;
-            Vec3 MountDirection = peer.ControlledAgent.MountAgent.GetMovementDirection().ToVec3();
 
             int damageAmount = 60; // Degat appliqué à la cible
             float radius = 3.5f; // Radius de recherche ennemi
+            float impactDistance = 1f; //Distance acceptable d'impact/hit (Squared value si >1)
 
-            //Debug
-            //Common.Utilities.Logger.SendMessageToAll($"Mount direction =  {MountDirection} , playerPosition {PlayerPosition}");
+            Agent nearbyAgent = CoreUtils.GetNearestAliveAgentInRange(radius, peer.ControlledAgent);
 
-            List<Agent> nearbyAllAgents = CoreUtils.GetNearAliveAgentsInRange(radius, peer.ControlledAgent);
-
-            foreach (var agent in nearbyAllAgents)
+            if (nearbyAgent != null && nearbyAgent != peer.ControlledAgent)
             {
-                if (agent != null && agent != peer.ControlledAgent)
+                //Debug
+                //Log($"Agent {agent.Name} position {agent.Position}", LogLevel.Debug);
+                //Common.Utilities.Logger.SendMessageToAll($"Nom agent =  {nearbyAgent.Name} et agent Distance {PlayerPosition.Distance(nearbyAgent.Position)}");                    
+
+                //Calcul hauteur pour ne pas tuer un agent trop haut ou trop bas
+                float peerZAxe = peer.ControlledAgent.MountAgent.Position.z;
+                float diffHauteur = Math.Abs(peerZAxe - nearbyAgent.Position.z);
+
+                //boneType = 36 en dure pour le moment, evol à faire pour ajouter une liste
+                bool check = await CheckBoneDuringAnimation(peer.ControlledAgent.MountAgent, nearbyAgent, 36, 1, impactDistance);
+                if (check && diffHauteur < 2f)
                 {
-                    //Debug
-                    //Log($"Agent {agent.Name} position {agent.Position}", LogLevel.Debug);
-                    //Common.Utilities.Logger.SendMessageToAll($"Nom agent =  {agent.Name} et agent position = {agent.Position}");                    
-
-                    //Calcul du Vecteur entre position joueur et position ennemi                    
-                    Vec3 PlayerToAgent = (PlayerPosition - agent.Position).NormalizedCopy();
-
-                    // Calculer l'angle entre la direction du joueur Peer et la direction de l'agent cible
-                    float angleInRadians = (float)Math.Atan2(PlayerToAgent.y * MountDirection.x - PlayerToAgent.x * MountDirection.y, PlayerToAgent.x * MountDirection.x + PlayerToAgent.y * MountDirection.y);
-                    float angleInDegrees = (float)(angleInRadians * (180f / Math.PI));
-
-                    //Calcul hauteur pour ne pas tuer un agent trop haut ou trop bas
-                    float peerZAxe = peer.ControlledAgent.MountAgent.Position.z;
-                    float diffHauteur = Math.Abs(peerZAxe - agent.Position.z);
-
-                    //Debug
-                    //Log($"Angle =  {angleInDegrees}", LogLevel.Debug);
-                    //Common.Utilities.Logger.SendMessageToAll($"Angle =  {angleInDegrees}");                    
-
-                    //Devant = -180 ; Gauche = -90; Droite = 90; Derriere = 0                    
-                    string position = string.Empty;
-                    if (angleInDegrees < -155f && angleInDegrees > -180f && diffHauteur < 2f)
-                    {
-                        position = "Front Left";
-                        // Common.Utilities.Logger.SendMessageToAll($"Position calculée =  {position}");
-                        DealDamage(agent, damageAmount, agent.Position, peer.ControlledAgent);
-                    }
-                    else if (angleInDegrees < 0f && angleInDegrees > -90f)
-                    {
-                        position = "Back Left";
-                        //Common.Utilities.Logger.SendMessageToAll($"Position calculée =  {position}");
-                    }
-                    else if (angleInDegrees < 90f && angleInDegrees > 0f)
-                    {
-                        position = "Back Right";
-                        //Common.Utilities.Logger.SendMessageToAll($"Position calculée =  {position}");
-                    }
-                    else if ((angleInDegrees > 155f || angleInDegrees < -180f) && diffHauteur < 2f)
-                    {
-                        position = "Front Right";
-                        //Common.Utilities.Logger.SendMessageToAll($"Position calculée =  {position}");
-                        DealDamage(agent, damageAmount, agent.Position, peer.ControlledAgent);
-                    }
-                    else
-                    {
-                        position = "error ?";
-                    }
-
+                    System.Diagnostics.Debug.WriteLine($"DANS MAIN APPLIQUER DES DEGATS");
+                    DealDamage(nearbyAgent, damageAmount, peer.ControlledAgent);
                 }
+                else
+                {
+                    System.Diagnostics.Debug.WriteLine($"PAS de degat applique dans MAIN");
+                }
+
             }
         }
 
-        // <summary>
-        /// Apply damage to an agent. 
+        /// <summary>
+        /// Find the first bone in selected range
         /// </summary>
-        /// <param name="agent">The agent that will be damaged</param>
-        /// <param name="damageAmount">How much damage the agent will receive.</param>
-        /// <param name="damager">The agent who is applying the damage</param>
-        public void DealDamage(Agent agent, int damageAmount, Vec3 impactPosition, Agent damager = null)
+        /// <param name="agent">Player mount agent</param>
+        /// <param name="target">Target</param>
+        /// <param name="boneType">ID of the agent bone to compare distance with target</param>
+        /// <param name="rangeSquarded">Range square of maximum allowed range</param>
+        /// <returns>Bone id if in range else -1</returns>
+        public static sbyte SearchBoneInRange(Agent agent, Agent target, sbyte boneType, float rangeSquarded)
+        {
+            System.Diagnostics.Debug.WriteLine($"J'entre dans searchBoneInRange");
+            MatrixFrame agentGlobalFrame = agent.AgentVisuals.GetGlobalFrame(); // recupère frame dans scene globale
+            MatrixFrame cibleGlobalFrame = target.AgentVisuals.GetGlobalFrame(); // recupère frame dans scene globale          
+            MatrixFrame agentBone = agent.AgentVisuals.GetSkeleton().GetBoneEntitialFrameWithIndex(boneType); // Récupère info du bone patte warg Fingers2_R (36)                                                                                                             
+
+            Vec3 agentBoneGlobalPosition = agentGlobalFrame.TransformToParent(agentBone.origin);//Transpose le bone agent(scene agent) dans la scene mission
+
+            sbyte closestBone = -1;
+            float closestDistanceSquared = float.MaxValue;
+
+            int targetBoneCount = target.AgentVisuals.GetSkeleton().GetBoneCount();
+
+            System.Diagnostics.Debug.WriteLine($"Début de la recherche de bone le plus proche. Nombre de bones dans la cible: {targetBoneCount}");
+            System.Diagnostics.Debug.WriteLine($"debut du FOR");
+            for (int i = 0; i < targetBoneCount; i++)
+            {
+                //Get bone frame, then set it to scene coordinate
+                MatrixFrame targetBoneFrame = target.AgentVisuals.GetSkeleton().GetBoneEntitialFrameWithIndex((sbyte)i);
+                Vec3 targetBoneGlobalPosition = cibleGlobalFrame.TransformToParent(targetBoneFrame.origin);
+
+                float distanceSquared = (targetBoneGlobalPosition - agentBoneGlobalPosition).LengthSquared;
+
+                System.Diagnostics.Debug.WriteLine($"Bone ID: {i}, Position: {targetBoneGlobalPosition}, Distance au carré: {distanceSquared}");
+
+                if (distanceSquared < closestDistanceSquared)
+                {
+                    closestDistanceSquared = distanceSquared;
+                    closestBone = (sbyte)i;//Keep it just in case for future use...               
+
+                    System.Diagnostics.Debug.WriteLine($"Nouveau bone le plus proche: {closestBone}, Distance au carré: {closestDistanceSquared}");
+                }
+
+                System.Diagnostics.Debug.WriteLine($"DistancesSquared: {distanceSquared}, rangeSquarded : {rangeSquarded}");
+                if (distanceSquared <= rangeSquarded)
+                {
+                    System.Diagnostics.Debug.WriteLine($"Bone ID: {i} est dans la plage de distance requise. Retourne {closestBone}");
+                    return closestBone;
+                }
+            }
+
+            // DEBUG : Affiche les résultats
+            if (closestBone == -1)
+            {
+                Common.Utilities.Logger.SendMessageToAll("Aucun bone trouvé .");
+                System.Diagnostics.Debug.WriteLine("Aucun bone trouvé dans la cible.");
+            }
+            System.Diagnostics.Debug.WriteLine("Arrive a la fin de search sans resultat");
+            return closestBone = -1;
+        }
+
+        /// <summary>
+        /// Find a bone to hit during animation
+        /// </summary>
+        /// <param name="agent">Player Mount</param>
+        /// <param name="target">Closest target</param>
+        /// <param name="agentBoneType">ID of the bone needed to hit the target</param>
+        /// <param name="duration">duration of animation</param>
+        /// <param name="minDistanceSquared">minimal distance to hit the target</param>
+        /// <returns>True if target bone found in range else false</returns>
+        public static async Task<bool> CheckBoneDuringAnimation(Agent agent, Agent target, sbyte agentBoneType, int duration, float minDistanceSquared)
         {
 
-            if (agent == null || !agent.IsActive())
+            float startTime = Mission.Current.CurrentTime;
+            //float endTime = startTime + duration;
+            try
+            {
+
+                while (Mission.Current.CurrentTime - startTime < duration)
+                {
+                    System.Diagnostics.Debug.WriteLine($"CurrentTime = {Mission.Current.CurrentTime - startTime} et EndTime = {duration}");
+                    if (agent == null || target == null || !agent.IsActive() || !target.IsActive())
+                    {
+                        Common.Utilities.Logger.SendMessageToAll("Agent ou cible invalide.");
+                        System.Diagnostics.Debug.WriteLine("Agent or target is null or inactive.");
+                        return false;
+                    }
+
+                    sbyte closestBone = SearchBoneInRange(agent, target, agentBoneType, minDistanceSquared);
+
+                    // Vérifier si un bone a été trouvé
+                    if (closestBone == -1)
+                    {
+                        System.Diagnostics.Debug.WriteLine("Aucun bone trouvé à la bonne distance lors de la recherche.");
+                        await Task.Delay(400); // Attendre avant de réessayer
+                        continue;
+                    }
+                    else
+                    {
+                        return true;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Common.Utilities.Logger.SendMessageToAll($"Exception: {ex.Message}");
+                return false;
+            }
+
+            // Si la distance minimale n'a pas été atteinte pendant la durée de l'animation
+            Common.Utilities.Logger.SendMessageToAll("Animation terminée, distance minimale non atteinte.");
+            return false;
+        }
+
+        /// <summary>
+        /// Apply damage to an agent
+        /// </summary>
+        /// <param name="target">The agent that will be hit</param>
+        /// <param name="damageAmount">How much damage the agent will receive</param>
+        /// <param name="damager">The agent who is applying the damage</param>
+        public void DealDamage(Agent target, int damageAmount, Agent damager = null)
+        {
+
+            if (target == null || !target.IsActive())
             {
                 Log("DealDamage: attempted to apply damage to a null or dead agent.", LogLevel.Warning);
                 return;
             }
             try
             {
-                if (agent.State == AgentState.Active || agent.State == AgentState.Routed)
+                if (target.State == AgentState.Active || target.State == AgentState.Routed)
                 {
-                    if (agent.IsFadingOut())
+                    if (target.IsFadingOut())
                         return;
 
-                    var damagerAgent = damager != null ? damager : agent;
+                    var damagerAgent = damager != null ? damager : target;
 
-                    CoreUtils.TakeDamage(agent, damagerAgent, damageAmount); // Application du blow
+                    CoreUtils.TakeDamage(target, damagerAgent, damageAmount); // Application du blow
                 }
             }
             catch (Exception e)
             {
                 Log($"Impossible d'appliquer des dommages via DealDamage pour le Warg. Voir {e.Message}", LogLevel.Error);
-                GameNetwork.BeginModuleEventAsServer(agent.MissionPeer.GetNetworkPeer());
+                GameNetwork.BeginModuleEventAsServer(target.MissionPeer.GetNetworkPeer());
                 GameNetwork.WriteMessage(new SendNotification("Une erreur d'application de degat pour le Warg est survenue !", 0));
                 GameNetwork.EndModuleEventAsServer();
             }
