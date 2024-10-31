b138d4041a081b48a75f227e4049798874f095f3
Spawn <pbbw@live.fr>
2024-10-31 16:30:47 +0100

    Fin de test condition mort agent spécifique

--- Alliance.Common/GameModes/Story/Conditions/AgentDeathCondition.cs
+++ Alliance.Common/GameModes/Story/Conditions/AgentDeathCondition.cs
@@ -1,5 +1,6 @@
 ﻿using Alliance.Common.GameModes.Story.Behaviors;
 using Alliance.Common.GameModes.Story.Utilities;
+using TaleWorlds.Core;
 using TaleWorlds.MountAndBlade;
 
 namespace Alliance.Common.GameModes.Story.Conditions
@@ -12,18 +13,17 @@ namespace Alliance.Common.GameModes.Story.Conditions
 
 		[ScenarioEditor(label: "Character", tooltip: "ID of the character to check death.")]
 		public string Character;
+		public SideType Side = SideType.All;
 		[ScenarioEditor(label: "Death quota", tooltip: "How many time character need to die to trigger condition.")]
 		public int DeathQuota = 1;
 		[ScenarioEditor(label: "Repeat", tooltip: "If true, it will trigger each time Death quota is reached once again.")]
 		public bool Repeat;
-
 
 		private Agent _DeadAgent;
 		private int _DeathCount = 0 ;
 
 		public override void Register()
 		{
-			//Mission.Current.GetMissionBehavior<RespawnBehavior>().RespawnPlayer(playerSelected);
 			ConditionsBehavior cdtBehavior = Mission.Current.GetMissionBehavior<ConditionsBehavior>();
 			if (cdtBehavior == null) return;
 			cdtBehavior.UpdateAgentDeathCondition += CheckdeadagentId;
@@ -39,7 +39,7 @@ namespace Alliance.Common.GameModes.Story.Conditions
 		private void CheckdeadagentId(Agent victim)
 		{
 			_DeadAgent = victim;
-			if (Character == _DeadAgent.Character.StringId)
+			if (Character == _DeadAgent.Character.StringId & (Side == SideType.All || (int)victim.Team.Side == (int)Side) )
 			{
 				_DeathCount += 1;
 				_DeadAgent = default;
