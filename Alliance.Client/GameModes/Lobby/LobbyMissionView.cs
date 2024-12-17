23859d88b0202355a4fde8125082e94c2ccc0c94
spawnpbbw <pbbw@live.fr>
2024-12-17 19:07:44 +0100

    Adding Race Condition on item for picking up (#46)
    * Adding Race Condition on item for picking up
    
    * Remove ToString as suggested in pull request. Tested and validated
    
    * Pull request, adding commentary for clarification

--- Alliance.Client/GameModes/Lobby/LobbyMissionView.cs
+++ Alliance.Client/GameModes/Lobby/LobbyMissionView.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using Alliance.Client.Extensions.ExNativeUI.AgentStatus.Views;
 using TaleWorlds.Library;
 using TaleWorlds.MountAndBlade;
 using TaleWorlds.MountAndBlade.Multiplayer.View.MissionViews;
@@ -17,9 +18,11 @@ namespace Alliance.Client.GameModes.Lobby
 			List<MissionView> views = DefaultViews.GetDefaultViews(mission, "Lobby");
 			views.AppendList(new List<MissionView>
 			{
+				// Custom views
+				new AgentStatusView(),
+				
 				// Native views
 				MultiplayerViewCreator.CreateMissionMultiplayerFFAView(),
-				ViewCreator.CreateMissionAgentStatusUIHandler(mission),
 				MultiplayerViewCreator.CreateMissionScoreBoardUIHandler(mission, false),
 				MultiplayerViewCreator.CreateMultiplayerMissionDeathCardUIHandler()
 			});
