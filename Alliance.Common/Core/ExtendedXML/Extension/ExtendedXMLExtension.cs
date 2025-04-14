39a5ec78fe37c5735589713e2e56995d4e7bf460
Byako <byako.dev@outlook.fr>
2025-04-14 21:30:42 +0200

    Use a default ExtendedCharacter when none is defined.

--- Alliance.Common/Core/ExtendedXML/Extension/ExtendedXMLExtension.cs
+++ Alliance.Common/Core/ExtendedXML/Extension/ExtendedXMLExtension.cs
@@ -2,34 +2,40 @@
 using System.Collections.Generic;
 using TaleWorlds.Core;
 using TaleWorlds.ObjectSystem;
+using static Alliance.Common.Utilities.Logger;
 
 namespace Alliance.Common.Core.ExtendedXML.Extension
 {
-    public static class ExtendedXMLExtension
-    {
-        private static readonly Dictionary<string, ExtendedCharacter> ExtendedCharacterCache = new Dictionary<string, ExtendedCharacter>();
-        private static readonly Dictionary<string, ExtendedItem> ExtendedItemCache = new Dictionary<string, ExtendedItem>();
+	public static class ExtendedXMLExtension
+	{
+		private static readonly Dictionary<string, ExtendedCharacter> ExtendedCharacterCache = new Dictionary<string, ExtendedCharacter>();
+		private static readonly Dictionary<string, ExtendedItem> ExtendedItemCache = new Dictionary<string, ExtendedItem>();
 
-        public static ExtendedCharacter GetExtendedCharacterObject(this BasicCharacterObject basicCharacterObject)
-        {
-            string key = "NPCCharacter." + basicCharacterObject.StringId;
-            if (!ExtendedCharacterCache.TryGetValue(key, out ExtendedCharacter cachedCharacter))
-            {
-                cachedCharacter = MBObjectManager.Instance.GetObject<ExtendedCharacter>(key);
-                ExtendedCharacterCache[key] = cachedCharacter;
-            }
-            return cachedCharacter;
-        }
+		public static ExtendedCharacter GetExtendedCharacterObject(this BasicCharacterObject basicCharacterObject)
+		{
+			string key = "NPCCharacter." + basicCharacterObject.StringId;
+			if (!ExtendedCharacterCache.TryGetValue(key, out ExtendedCharacter cachedCharacter))
+			{
+				cachedCharacter = MBObjectManager.Instance.GetObject<ExtendedCharacter>(key);
+				if (cachedCharacter == null)
+				{
+					Log($"No ExtendedCharacter defined for {basicCharacterObject.StringId}, using default one", LogLevel.Debug);
+					cachedCharacter = new ExtendedCharacter(basicCharacterObject);
+				}
+				ExtendedCharacterCache[key] = cachedCharacter;
+			}
+			return cachedCharacter;
+		}
 
-        public static ExtendedItem GetExtendedItem(this ItemObject itemObject)
-        {
-            string key = "item." + itemObject.StringId;
-            if (!ExtendedItemCache.TryGetValue(key, out ExtendedItem cachedItem))
-            {
-                cachedItem = MBObjectManager.Instance.GetObject<ExtendedItem>(key);
-                ExtendedItemCache[key] = cachedItem;
-            }
-            return cachedItem;
-        }
-    }
+		public static ExtendedItem GetExtendedItem(this ItemObject itemObject)
+		{
+			string key = "item." + itemObject.StringId;
+			if (!ExtendedItemCache.TryGetValue(key, out ExtendedItem cachedItem))
+			{
+				cachedItem = MBObjectManager.Instance.GetObject<ExtendedItem>(key);
+				ExtendedItemCache[key] = cachedItem;
+			}
+			return cachedItem;
+		}
+	}
 }
