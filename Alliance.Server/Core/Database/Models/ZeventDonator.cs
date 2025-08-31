e676780f6516f413f745aa005fefd67f23bbbab9
P-H Lemp <pierrehenrilempereur1997@gmail.com>
2025-08-31 19:02:45 +0200

    Add ZeventDonator

--- Alliance.Server/Core/Database/Models/ZeventDonator.cs
+++ Alliance.Server/Core/Database/Models/ZeventDonator.cs
@@ -3,24 +3,24 @@ using System.Collections.Generic;
 
 namespace Alliance.Server.Core.Database.Models
 {
-    /// <summary>
-    /// Table des donateurs
-    /// </summary>
-    public partial class ZeventDonator
-    {
-        public ZeventDonator()
-        {
-            ZeventDonations = new HashSet<ZeventDonation>();
-            ZeventRewards = new HashSet<ZeventReward>();
-        }
+	/// <summary>
+	/// Table des donateurs
+	/// </summary>
+	public partial class ZeventDonator
+	{
+		public ZeventDonator()
+		{
+			ZeventDonations = new HashSet<ZeventDonation>();
+			ZeventRewards = new HashSet<ZeventReward>();
+		}
 
-        public int Id { get; set; }
-        public DateTime InsertDate { get; set; }
-        public DateTime LastUpdateDate { get; set; }
-        public DateTime? DeletedAt { get; set; }
-        public string Username { get; set; }
+		public int Id { get; set; }
+		public DateTime InsertDate { get; set; }
+		public DateTime LastUpdateDate { get; set; }
+		public DateTime? DeletedAt { get; set; }
+		public string Username { get; set; }
 
-        public virtual ICollection<ZeventDonation> ZeventDonations { get; set; }
-        public virtual ICollection<ZeventReward> ZeventRewards { get; set; }
-    }
+		public virtual ICollection<ZeventDonation> ZeventDonations { get; set; }
+		public virtual ICollection<ZeventReward> ZeventRewards { get; set; }
+	}
 }
