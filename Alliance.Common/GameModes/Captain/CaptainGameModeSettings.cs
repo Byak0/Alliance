477f6c1f3e608b4e386a60e856bcfdad316f4861
Byako <byako.dev@outlook.fr>
2025-09-16 23:46:15 +0200

    Change available options for some game modes.

--- Alliance.Common/GameModes/Captain/CaptainGameModeSettings.cs
+++ Alliance.Common/GameModes/Captain/CaptainGameModeSettings.cs
@@ -5,50 +5,52 @@ using static TaleWorlds.MountAndBlade.MultiplayerOptions;
 
 namespace Alliance.Common.GameModes.Captain
 {
-    public class CaptainGameModeSettings : GameModeSettings
-    {
-        public CaptainGameModeSettings() : base("CaptainX", "Captain", "Captain")
-        {
-        }
+	public class CaptainGameModeSettings : GameModeSettings
+	{
+		public CaptainGameModeSettings() : base("CaptainX", "Captain", "Captain")
+		{
+		}
 
-        public override void SetDefaultNativeOptions()
-        {
-            base.SetDefaultNativeOptions();
-        }
+		public override void SetDefaultNativeOptions()
+		{
+			base.SetDefaultNativeOptions();
+		}
 
-        public override void SetDefaultModOptions()
-        {
-            base.SetDefaultModOptions();
-        }
+		public override void SetDefaultModOptions()
+		{
+			base.SetDefaultModOptions();
+		}
 
-        public override List<SceneInfo> GetAvailableMaps()
-        {
-            return base.GetAvailableMaps().Where(scene => scene.HasSpawnForAttacker && scene.HasSpawnForDefender && scene.HasSpawnVisual && scene.HasNavmesh).ToList();
-        }
+		public override List<SceneInfo> GetAvailableMaps()
+		{
+			return base.GetAvailableMaps().Where(scene => scene.HasSpawnForAttacker && scene.HasSpawnForDefender && scene.HasSpawnVisual && scene.HasNavmesh).ToList();
+		}
 
-        public override List<OptionType> GetAvailableNativeOptions()
-        {
-            return new List<OptionType>
-            {
-                OptionType.CultureTeam1,
-                OptionType.CultureTeam2,
-                OptionType.NumberOfBotsPerFormation,
-                OptionType.NumberOfBotsTeam1,
-                OptionType.NumberOfBotsTeam2,
-                OptionType.RoundPreparationTimeLimit,
-                OptionType.RoundTimeLimit,
-                OptionType.RoundTotal,
-                OptionType.WarmupTimeLimit,
-                OptionType.FriendlyFireDamageMeleeFriendPercent,
-                OptionType.FriendlyFireDamageMeleeSelfPercent,
-                OptionType.FriendlyFireDamageRangedFriendPercent,
-                OptionType.FriendlyFireDamageRangedSelfPercent
-            };
-        }
+		public override List<OptionType> GetAvailableNativeOptions()
+		{
+			return new List<OptionType>
+			{
+				OptionType.CultureTeam1,
+				OptionType.CultureTeam2,
+				OptionType.NumberOfBotsPerFormation,
+				OptionType.NumberOfBotsTeam1,
+				OptionType.NumberOfBotsTeam2,
+				OptionType.AutoTeamBalanceThreshold,
+				OptionType.WarmupTimeLimitInSeconds,
+				OptionType.RoundPreparationTimeLimit,
+				OptionType.RoundTimeLimit,
+				OptionType.RoundTotal,
+				OptionType.UnlimitedGold,
+				OptionType.FriendlyFireDamageMeleeFriendPercent,
+				OptionType.FriendlyFireDamageMeleeSelfPercent,
+				OptionType.FriendlyFireDamageRangedFriendPercent,
+				OptionType.FriendlyFireDamageRangedSelfPercent
+			};
+		}
 
-        public override List<string> GetAvailableModOptions()
-        {
-            return base.GetAvailableModOptions();
-        }
-    }
+		public override List<string> GetAvailableModOptions()
+		{
+			return base.GetAvailableModOptions();
+		}
+	}
 }
--- Alliance.Common/GameModes/Story/ScenarioGameModeSettings.cs
+++ Alliance.Common/GameModes/Story/ScenarioGameModeSettings.cs
@@ -1,54 +1,41 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
 using static Alliance.Common.Utilities.SceneList;
 using static TaleWorlds.MountAndBlade.MultiplayerOptions;
 
 namespace Alliance.Common.GameModes.Story
 {
-    public class ScenarioGameModeSettings : GameModeSettings
-    {
-        public ScenarioGameModeSettings() : base("Scenario", "Scenario", "Play a premade scenario.")
-        {
-        }
+	public class ScenarioGameModeSettings : GameModeSettings
+	{
+		public ScenarioGameModeSettings() : base("Scenario", "Scenario", "Play a premade scenario.")
+		{
+		}
 
-        public override void SetDefaultNativeOptions()
-        {
-            base.SetDefaultNativeOptions();
-        }
+		public override void SetDefaultNativeOptions()
+		{
+			base.SetDefaultNativeOptions();
+		}
 
-        public override void SetDefaultModOptions()
-        {
-            base.SetDefaultModOptions();
-            ModOptions.KillFeedEnabled = false;
-            ModOptions.ShowScore = false;
-            ModOptions.ShowOfficers = false;
-        }
+		public override void SetDefaultModOptions()
+		{
+			base.SetDefaultModOptions();
+			ModOptions.KillFeedEnabled = false;
+			ModOptions.ShowScore = false;
+			ModOptions.ShowOfficers = false;
+		}
 
-        public override List<SceneInfo> GetAvailableMaps()
-        {
-            return base.GetAvailableMaps().Where(scene => scene.HasSpawnForAttacker && scene.HasSpawnForDefender).ToList();
-        }
+		public override List<SceneInfo> GetAvailableMaps()
+		{
+			return new List<SceneInfo>();
+		}
 
-        public override List<OptionType> GetAvailableNativeOptions()
-        {
-            return new List<OptionType>
-            {
-                OptionType.CultureTeam1,
-                OptionType.CultureTeam2,
-                OptionType.NumberOfBotsTeam1,
-                OptionType.NumberOfBotsTeam2,
-                OptionType.RoundPreparationTimeLimit,
-                OptionType.MapTimeLimit,
-                OptionType.FriendlyFireDamageMeleeFriendPercent,
-                OptionType.FriendlyFireDamageMeleeSelfPercent,
-                OptionType.FriendlyFireDamageRangedFriendPercent,
-                OptionType.FriendlyFireDamageRangedSelfPercent
-            };
-        }
+		public override List<OptionType> GetAvailableNativeOptions()
+		{
+			return new List<OptionType>();
+		}
 
-        public override List<string> GetAvailableModOptions()
-        {
-            return base.GetAvailableModOptions();
-        }
-    }
+		public override List<string> GetAvailableModOptions()
+		{
+			return new List<string>();
+		}
+	}
 }
