f93e8bf384808ef3e569ff71ad2f007ab23c5f0e
Byako <byako.dev@outlook.fr>
2025-10-22 22:50:59 +0200

    Update BR type

--- Alliance.Common/GameModes/BattleRoyale/Behaviors/BRCommonBehavior.cs
+++ Alliance.Common/GameModes/BattleRoyale/Behaviors/BRCommonBehavior.cs
@@ -4,60 +4,60 @@ using TaleWorlds.MountAndBlade;
 
 namespace Alliance.Common.GameModes.BattleRoyale.Behaviors
 {
-    public class BRCommonBehavior : MissionMultiplayerGameModeBaseClient, IBotControllerBehavior
-    {
-        public override bool IsGameModeUsingGold
-        {
-            get
-            {
-                return true;
-            }
-        }
+	public class BRCommonBehavior : MissionMultiplayerGameModeBaseClient, IBotControllerBehavior
+	{
+		public override bool IsGameModeUsingGold
+		{
+			get
+			{
+				return true;
+			}
+		}
 
-        public override bool IsGameModeTactical
-        {
-            get
-            {
-                return false;
-            }
-        }
+		public override bool IsGameModeTactical
+		{
+			get
+			{
+				return false;
+			}
+		}
 
-        public override bool IsGameModeUsingRoundCountdown
-        {
-            get
-            {
-                return false;
-            }
-        }
+		public override bool IsGameModeUsingRoundCountdown
+		{
+			get
+			{
+				return false;
+			}
+		}
 
-        public override MultiplayerGameType GameType
-        {
-            get
-            {
-                return MultiplayerGameType.FreeForAll;
-            }
-        }
+		public override MultiplayerGameType GameType
+		{
+			get
+			{
+				return MultiplayerGameType.TeamDeathmatch;
+			}
+		}
 
-        public override int GetGoldAmount()
-        {
-            return 2000;
-        }
+		public override int GetGoldAmount()
+		{
+			return 2000;
+		}
 
-        public override void OnGoldAmountChangedForRepresentative(MissionRepresentativeBase representative, int goldAmount)
-        {
-        }
+		public override void OnGoldAmountChangedForRepresentative(MissionRepresentativeBase representative, int goldAmount)
+		{
+		}
 
-        public override void AfterStart()
-        {
-            Mission.SetMissionMode(MissionMode.Battle, true);
-        }
+		public override void AfterStart()
+		{
+			Mission.SetMissionMode(MissionMode.Battle, true);
+		}
 
-        public void OnBotsControlledChanged(MissionPeer component, int aliveCount, int totalCount)
-        {
-        }
+		public void OnBotsControlledChanged(MissionPeer component, int aliveCount, int totalCount)
+		{
+		}
 
-        public BRCommonBehavior()
-        {
-        }
-    }
+		public BRCommonBehavior()
+		{
+		}
+	}
 }
