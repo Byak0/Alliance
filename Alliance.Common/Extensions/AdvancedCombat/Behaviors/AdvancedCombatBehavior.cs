3bddd6b3d971be58e5ed27c0623f213d12e8816b
Byako <byako.dev@outlook.fr>
2026-01-09 21:39:42 +0100

    Update methods signatures

--- Alliance.Common/Extensions/AdvancedCombat/Behaviors/AdvancedCombatBehavior.cs
+++ Alliance.Common/Extensions/AdvancedCombat/Behaviors/AdvancedCombatBehavior.cs
@@ -73,6 +73,7 @@ namespace Alliance.Common.Extensions.AdvancedCombat.Behaviors
 				{
 					continue;
 				}
+
 				List<AL_DefaultAgentComponent> components = agent.Components.Where(component => component is AL_DefaultAgentComponent).Select(component => component as AL_DefaultAgentComponent).ToList();
 				float offset_Z = agent.HasMount ? 1f : 0f;
 				foreach (AL_DefaultAgentComponent component in components)
