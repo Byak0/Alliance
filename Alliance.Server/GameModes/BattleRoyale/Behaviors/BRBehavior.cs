f50bc72bac3c84b9e269ab1f902d56608b9926a9
Byako <6892911+Byak0@users.noreply.github.com>
2026-06-01 22:20:39 +0200

    Fix crash on BR end (was launching a missing map)

--- Alliance.Server/GameModes/BattleRoyale/Behaviors/BRBehavior.cs
+++ Alliance.Server/GameModes/BattleRoyale/Behaviors/BRBehavior.cs
@@ -116,7 +116,7 @@ namespace Alliance.Server.GameModes.BattleRoyale.Behaviors
 					CommonAdminMsg.SendNotificationToAll(loseMessage);
 					Log(loseMessage);
 				}
-				GameModeStarter.Instance.StartLobby("Lobby", MultiplayerOptions.OptionType.CultureTeam1.GetStrValue(), MultiplayerOptions.OptionType.CultureTeam2.GetStrValue());
+				GameModeStarter.Instance.StartLobby(MultiplayerOptions.OptionType.Map.GetStrValue(), MultiplayerOptions.OptionType.CultureTeam1.GetStrValue(), MultiplayerOptions.OptionType.CultureTeam2.GetStrValue());
 				_gameEnded = true;
 			}
 		}
